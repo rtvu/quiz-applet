897514e31ca304b1f45f8bd00c231fa285f98672
ryan <rtvu87@gmail.com>
2024-06-11 19:59:16 +0000

    Convert html entities when parsing query

--- src/js/useQuestions.ts
+++ src/js/useQuestions.ts
@@ -1,5 +1,7 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 
+import { decode } from "html-entities";
+
 async function fetchQuestions() {
   const result = await fetch("https://opentdb.com/api.php?amount=5&category=22&difficulty=easy&type=multiple");
 
@@ -19,6 +21,19 @@ export type Question = {
 export function useQuestions(round: number): [Question[], UseQueryResult["status"]] {
   const results = useQuery({ queryKey: ["questions", round], queryFn: fetchQuestions });
 
+  let questions: Question[] = [];
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  return [(results.data?.results ?? []) as Question[], results.status];
+  if (results.data?.results) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const data = results.data.results as Question[];
+    questions = data.map((question) => {
+      return {
+        question: decode(question.question),
+        correct_answer: decode(question.correct_answer),
+        incorrect_answers: question.incorrect_answers.map((answer) => decode(answer)),
+      };
+    });
+  }
+
+  return [questions, results.status];
 }
