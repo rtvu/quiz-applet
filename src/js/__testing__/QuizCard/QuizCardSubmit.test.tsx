4482ba93db3f4b98cbcf41c5cd6e17dab7a422f0
ryan <rtvu87@gmail.com>
2024-06-13 07:40:41 +0000

    Updated test for `QuizCardSubmit`

--- src/js/__testing__/QuizCard/QuizCardSubmit.test.tsx
+++ src/js/__testing__/QuizCard/QuizCardSubmit.test.tsx
@@ -12,7 +12,7 @@ describe("QuizCardSubmit", () => {
         disabled
         id={id}
         onSubmit={() => {
-          console.log();
+          undefined;
         }}
       />,
     );
@@ -23,16 +23,18 @@ describe("QuizCardSubmit", () => {
 
   test("click button", async () => {
     const initialCount = 0;
+    const afterClickCount = 1;
+
     let count = initialCount;
 
     const onSubmit = () => {
-      count = 1;
+      count = afterClickCount;
     };
 
     const quizCardSubmit = render(<QuizCardSubmit id={id} onSubmit={onSubmit} />);
 
     const button = await quizCardSubmit.findByTestId(id);
     fireEvent.click(button);
-    expect(count).not.toBe(initialCount);
+    expect(count).toBe(afterClickCount);
   });
 });
