be8e3cfca9eb8c2c78fd2b17be59559a01d8c33c
ryan <rtvu87@gmail.com>
2024-06-16 09:10:40 +0000

    Updated component and tests for `QuizCardAnswers`

--- src/js/QuizCard/QuizCardAnswers.tsx
+++ src/js/QuizCard/QuizCardAnswers.tsx
@@ -1,48 +1,57 @@
-import { ReactNode, useId } from "react";
+import { useId, KeyboardEvent, ReactNode } from "react";
 
 export type QuizCardAnswersProps = {
   answers: string[];
   selection: number;
   onSelection: (index: number) => void;
+  groupName?: string;
   id?: string;
 };
 
 export function QuizCardAnswers(props: QuizCardAnswersProps): ReactNode {
-  const groupName = useId();
+  const idForGroupName = useId();
+  const groupName = props.groupName ?? idForGroupName;
 
-  const radios = props.answers.map((answer, index) => {
+  const Radios = props.answers.map((answer, index) => {
     const indexString = index.toString();
+
+    const onChangeOrClick = () => {
+      props.onSelection(index);
+    };
+
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        props.onSelection(index);
+      }
+    };
 
     return (
       <div key={index} className="input-group input-group-sm my-1">
         <div className="input-group-text">
           <input
-            className="form-check-input mt-0"
             type="radio"
-            aria-label={`Checkbox for answer choice: ${indexString}`}
+            className="form-check-input mt-0"
             name={groupName}
+            aria-label={`Checkbox for answer choice: ${indexString}`}
             value={index}
-            onChange={() => {
-              props.onSelection(index);
-            }}
-            checked={index === props.selection ? true : false}
-            data-testid={props.id !== undefined ? `${props.id}-radio-${indexString}` : undefined}
+            checked={index === props.selection}
+            onChange={onChangeOrClick}
+            data-testid={props.id === undefined ? undefined : `${props.id}-${indexString}-radio`}
           />
         </div>
         <input
           type="text"
           className="form-control"
-          aria-label={`Test for answer choice: ${indexString}`}
-          readOnly
+          aria-label={`Text for answer choice: ${indexString}`}
           value={answer}
-          onFocus={() => {
-            props.onSelection(index);
-          }}
-          data-testid={props.id !== undefined ? `${props.id}-text-${indexString}` : undefined}
+          readOnly
+          onClick={onChangeOrClick}
+          onKeyDown={onKeyDown}
+          data-testid={props.id === undefined ? undefined : `${props.id}-${indexString}-text`}
         />
       </div>
     );
   });
 
-  return <div className="card-body px-3 py-2">{radios}</div>;
+  return <div className="card-body px-3 py-2">{Radios}</div>;
 }
--- src/js/__testing__/QuizCard/QuizCardAnswers.test.tsx
+++ src/js/__testing__/QuizCard/QuizCardAnswers.test.tsx
@@ -3,37 +3,102 @@ import { fireEvent, render } from "@testing-library/react";
 
 import { QuizCardAnswers } from "../../QuizCard/QuizCardAnswers";
 
-describe("QuizCardAnswers", () => {
-  const id = "QuizCardAnswers";
+describe(QuizCardAnswers.name, () => {
+  const answers = ["Ryan", "Daniel", "Jack", "John"];
 
-  test("answers are selectable", async () => {
-    let selection = -1;
-    const answers = ["Ryan", "Daniel", "Jack", "John"];
-    const onSelection = (index: number) => {
-      selection = index;
-    };
+  let selection = -1;
+  const onSelection = (index: number) => {
+    selection = index;
+  };
 
+  const groupName = QuizCardAnswers.name;
+  const id = QuizCardAnswers.name;
+
+  test("radios are in the same group", async () => {
+    const quizCardAnswers = render(
+      <QuizCardAnswers
+        answers={answers}
+        selection={selection}
+        onSelection={onSelection}
+        groupName={groupName}
+        id={id}
+      />,
+    );
+
+    for (const index of answers.keys()) {
+      const indexString = index.toString();
+
+      const answerRadio = await quizCardAnswers.findByTestId(id + "-" + indexString + "-radio");
+      expect(answerRadio).toHaveAttribute("name", groupName);
+    }
+  });
+
+  test("answers are shown", async () => {
     const quizCardAnswers = render(
       <QuizCardAnswers answers={answers} selection={selection} onSelection={onSelection} id={id} />,
     );
 
     for (const [index, answer] of answers.entries()) {
       const indexString = index.toString();
-      const answerText = await quizCardAnswers.findByTestId(id + "-text-" + indexString);
+
+      const answerText = await quizCardAnswers.findByTestId(id + "-" + indexString + "-text");
       expect(answerText).toHaveDisplayValue(answer);
-      const answerRadio = await quizCardAnswers.findByTestId(id + "-radio-" + indexString);
-      expect(answerRadio).not.toBeChecked();
     }
+  });
+
+  test("selection checks answer", async () => {
+    selection = -1;
+
+    const quizCardAnswers = render(
+      <QuizCardAnswers answers={answers} selection={selection} onSelection={onSelection} id={id} />,
+    );
 
     for (const index of answers.keys()) {
       const indexString = index.toString();
-      const answerText = await quizCardAnswers.findByTestId(id + "-text-" + indexString);
-      selection = -1;
-      fireEvent.focus(answerText);
-      expect(selection).toBe(index);
-      const answerRadio = await quizCardAnswers.findByTestId(id + "-radio-" + indexString);
+
+      const answerRadio = await quizCardAnswers.findByTestId(id + "-" + indexString + "-radio");
+      expect(answerRadio).not.toBeChecked();
+    }
+
+    for (selection of answers.keys()) {
+      quizCardAnswers.rerender(
+        <QuizCardAnswers answers={answers} selection={selection} onSelection={onSelection} id={id} />,
+      );
+
+      for (const index of answers.keys()) {
+        const indexString = index.toString();
+
+        const answerRadio = await quizCardAnswers.findByTestId(id + "-" + indexString + "-radio");
+        if (selection === index) {
+          expect(answerRadio).toBeChecked();
+        } else {
+          expect(answerRadio).not.toBeChecked();
+        }
+      }
+    }
+  });
+
+  test("selecting answers changes selection", async () => {
+    selection = -1;
+
+    const quizCardAnswers = render(
+      <QuizCardAnswers answers={answers} selection={selection} onSelection={onSelection} id={id} />,
+    );
+
+    for (const index of answers.keys()) {
+      const indexString = index.toString();
+
+      const answerRadio = await quizCardAnswers.findByTestId(id + "-" + indexString + "-radio");
       selection = -1;
       fireEvent.click(answerRadio);
+      expect(selection).toBe(index);
+
+      const answerText = await quizCardAnswers.findByTestId(id + "-" + indexString + "-text");
+      selection = -1;
+      fireEvent.click(answerText);
+      expect(selection).toBe(index);
+      selection = -1;
+      fireEvent.keyDown(answerText, { key: "Enter", code: "Enter", charCode: 13 });
       expect(selection).toBe(index);
     }
   });
