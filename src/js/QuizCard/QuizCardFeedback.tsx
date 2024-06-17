97e76a1ddc223a10b929ad61bf8d8e49d83b9b16
ryan <rtvu87@gmail.com>
2024-06-17 23:40:58 +0000

    Updated component and tests for `QuizCardFeedback`

--- src/js/QuizCard/QuizCardFeedback.tsx
+++ src/js/QuizCard/QuizCardFeedback.tsx
@@ -11,33 +11,32 @@ export type QuizCardFeedbackProps = {
 
 export function QuizCardFeedback(props: QuizCardFeedbackProps): ReactNode {
   const Feedback = props.feedback.map(({ question, correct_answer, selected_answer }, index) => {
-    index = index + 1;
-
+    const indexString = index.toString();
     const isCorrect = correct_answer === selected_answer;
 
     return (
       <div key={index} className="d-flex my-1 px-2 py-1 border rounded bg-light">
         <p
           className="lh-sm my-0"
-          data-testid={props.id === undefined ? undefined : props.id + "-" + index.toString() + "-paragraph"}
+          data-testid={props.id === undefined ? undefined : `${props.id}-${indexString}-paragraph`}
         >
           <small
             className={isCorrect ? "text-success" : "text-danger"}
-            data-testid={props.id === undefined ? undefined : props.id + "-" + index.toString() + "-list-number"}
+            data-testid={props.id === undefined ? undefined : `${props.id}-${indexString}-list-number`}
           >
-            {index.toString()}.
+            {index + 1}.
           </small>
           <small>{` ${question} `}</small>
           <small
             className={isCorrect ? "text-success" : "text-danger"}
-            data-testid={props.id === undefined ? undefined : props.id + "-" + index.toString() + "-selected-answer"}
+            data-testid={props.id === undefined ? undefined : `${props.id}-${indexString}-selected-answer`}
           >
             {selected_answer}
           </small>
           {isCorrect ? null : (
             <small
               className="text-success"
-              data-testid={props.id === undefined ? undefined : props.id + "-" + index.toString() + "-correct-answer"}
+              data-testid={props.id === undefined ? undefined : `${props.id}-${indexString}-correct-answer`}
             >
               {" "}
               ({correct_answer})
--- src/js/__testing__/QuizCard/QuizCardFeedback.test.tsx
+++ src/js/__testing__/QuizCard/QuizCardFeedback.test.tsx
@@ -3,37 +3,35 @@ import { render } from "@testing-library/react";
 
 import { QuizCardFeedback, QuizCardFeedbackProps } from "../../QuizCard/QuizCardFeedback";
 
-describe("QuizCardFeedback", () => {
-  const id = "QuizCardFeedback";
-
+describe(QuizCardFeedback.name, () => {
   const feedback: QuizCardFeedbackProps["feedback"] = [
     { question: "What is your name?", correct_answer: "Ryan", selected_answer: "Ryan" },
     { question: "What is your name?", correct_answer: "Ryan", selected_answer: "Daniel" },
   ];
+  const id = QuizCardFeedback.name;
 
   test("correctly formats feedback", async () => {
     const quizCardFeedback = render(<QuizCardFeedback feedback={feedback} id={id} />);
 
     for (const [index, { question, correct_answer, selected_answer }] of feedback.entries()) {
+      const indexString = index.toString();
       const isCorrect = correct_answer === selected_answer;
 
-      const paragraph = await quizCardFeedback.findByTestId(id + "-" + (index + 1).toString() + "-paragraph");
+      const paragraph = await quizCardFeedback.findByTestId(`${id}-${indexString}-paragraph`);
+      expect(paragraph).toHaveTextContent(`${(index + 1).toString()}.`);
       expect(paragraph).toHaveTextContent(question);
       expect(paragraph).toHaveTextContent(selected_answer);
 
-      const listNumber = await quizCardFeedback.findByTestId(id + "-" + (index + 1).toString() + "-list-number");
+      const listNumber = await quizCardFeedback.findByTestId(`${id}-${indexString}-list-number`);
       expect(listNumber).toHaveClass(isCorrect ? "text-success" : "text-danger");
 
-      const selectedAnswer = await quizCardFeedback.findByTestId(
-        id + "-" + (index + 1).toString() + "-selected-answer",
-      );
+      const selectedAnswer = await quizCardFeedback.findByTestId(`${id}-${indexString}-selected-answer`);
       expect(selectedAnswer).toHaveClass(isCorrect ? "text-success" : "text-danger");
 
       if (!isCorrect) {
-        const correctAnswer = await quizCardFeedback.findByTestId(
-          id + "-" + (index + 1).toString() + "-correct-answer",
-        );
-        expect(correctAnswer).toHaveTextContent("(" + correct_answer + ")");
+        const correctAnswer = await quizCardFeedback.findByTestId(`${id}-${indexString}-correct-answer`);
+        expect(correctAnswer).toHaveTextContent(`(${correct_answer})`);
+        expect(correctAnswer).toHaveClass("text-success");
       }
     }
   });
