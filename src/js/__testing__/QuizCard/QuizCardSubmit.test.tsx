6696e7990ca96a7ad2e08beeaf04a69884d1fdc8
ryan <rtvu87@gmail.com>
2024-06-18 03:37:20 +0000

    Updated tests for `QuizCardSubmit`

--- src/js/__testing__/QuizCard/QuizCardSubmit.test.tsx
+++ src/js/__testing__/QuizCard/QuizCardSubmit.test.tsx
@@ -3,19 +3,15 @@ import { fireEvent, render } from "@testing-library/react";
 
 import { QuizCardSubmit } from "../../QuizCard/QuizCardSubmit";
 
-describe("QuizCardSubmit", () => {
-  const id = "QuizCardSubmit";
+describe(QuizCardSubmit.name, () => {
+  const id = QuizCardSubmit.name;
 
   test("disabled button", async () => {
-    const quizCardSubmit = render(
-      <QuizCardSubmit
-        disabled
-        id={id}
-        onSubmit={() => {
-          undefined;
-        }}
-      />,
-    );
+    const onSubmit = () => {
+      undefined;
+    };
+
+    const quizCardSubmit = render(<QuizCardSubmit disabled onSubmit={onSubmit} id={id} />);
 
     const button = await quizCardSubmit.findByTestId(id);
     expect(button).toBeDisabled();
@@ -26,12 +22,11 @@ describe("QuizCardSubmit", () => {
     const afterClickCount = 1;
 
     let count = initialCount;
-
     const onSubmit = () => {
       count = afterClickCount;
     };
 
-    const quizCardSubmit = render(<QuizCardSubmit id={id} onSubmit={onSubmit} />);
+    const quizCardSubmit = render(<QuizCardSubmit onSubmit={onSubmit} id={id} />);
 
     const button = await quizCardSubmit.findByTestId(id);
     fireEvent.click(button);
